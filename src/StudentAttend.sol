91c4a561ecb00ef359926d76d4257ab64d9055b6
Jaiyeola Akinjide <jaiyeolawety705@gmail.com>
2026-02-13 19:11:35 +0100

    update

--- src/StudentAttend.sol
+++ src/StudentAttend.sol
@@ -14,7 +14,7 @@ contract StudentAttendance {
     event StudentAdded(uint256 indexed studentId, string name, uint8 age);
     event AttendanceUpdated(uint256 indexed studentId, bool present);
 
-    function addStudent(string memory _name, uint8 _age) public {
+    function welcome_to_class(string memory _name, uint8 _age) public {
         students.push(Student({
             name: _name,
             age: _age,
@@ -24,7 +24,7 @@ contract StudentAttendance {
         emit StudentAdded(students.length - 1, _name, _age);
     }
 
-    function updateAttendance(uint256 _studentId, bool _present) public {
+    function take_attendance(uint256 _studentId, bool _present) public {
         require(_studentId < students.length, "Student does not exist");
         
         students[_studentId].present = _present;
@@ -32,11 +32,11 @@ contract StudentAttendance {
         emit AttendanceUpdated(_studentId, _present);
     }
 
-    function getTotalStudents() public view returns (uint256) {
+    function check_class_size() public view returns (uint256) {
         return students.length;
     }
 
-    function getStudent(uint256 _studentId) public view returns (string memory name, uint8 age, bool present) {
+    function get_student_info(uint256 _studentId) public view returns (string memory name, uint8 age, bool present) {
         require(_studentId < students.length, "Student does not exist");
         Student memory student = students[_studentId];
         return (student.name, student.age, student.present);
